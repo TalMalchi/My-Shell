1be3f1c60d727764f91f0206135ce0d8245d24b8
Tal <malchital@gmail.com>
2023-03-19 20:39:13 +0200

    echo, $? , cd, quit

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 #do make file for myshell2.cpp
 
 
-shell2: shell2.o
-	g++ -o shell2 shell2.o
+myshell: shell2.o
+	g++ -o myshell shell2.o
 
 shell2.o: shell2.cpp
 	g++ -c shell2.cpp
 
 clean:
-	rm -f shell2 file *.o
+	rm -f myshell file *.o
--- shell2.cpp
+++ shell2.cpp
@@ -6,12 +6,15 @@
 #include "stdlib.h"
 #include "unistd.h"
 #include <string.h>
+#include <iostream>
+
+using namespace std;
 
 int main() {
 char command[1024];
 char *token;
-char *outfile;
-int i, fd, amper, redirect, retid, status;
+char *outfile, *error_file ;
+int i, fd, amper, redirect_out, retid, status, redirect_appened, redirect_err, last_cmd_status;
 char *argv[10];
 
 while (1)
@@ -43,34 +46,96 @@ while (1)
     else 
         amper = 0; 
 
+    if (!strcmp(argv[0], "quit"))
+        exit(0);
+    
+    if (!strcmp(argv[0], "cd")) {
+        chdir(argv[1]);
+        continue;
+    }
+
+    if(!strcmp(argv[0], "echo") && !strcmp(argv[1], "$?")){
+        cout << last_cmd_status << endl;
+        continue;
+    }
+        
+
+    
+    if(!strcmp(argv[0], "echo")){
+        //print out the rest of the command line except for the echo command
+        for(int j = 1; j < i; j++){
+            cout << argv[j] << " ";
+        }
+        cout << endl; 
+        continue;
+    }
+
     if (! strcmp(argv[i - 2], ">")) {
-        redirect = 1;
+        redirect_out = 1;
+        redirect_appened = 0;
+        redirect_err = 0;
         argv[i - 2] = NULL;
         outfile = argv[i - 1];
+        cout << "outfile: " << outfile << endl;
+        
         }
+    else if (! strcmp(argv[i - 2], "2>")) {
+        redirect_out = 0;
+        redirect_appened = 0;
+        redirect_err = 1;
+        argv[i - 2] = NULL;
+        error_file = argv[i - 1];
+        cout << "error_file: " << error_file << endl;
+        }
+    else if (! strcmp(argv[i - 2], ">>")) {
+        redirect_out = 0;
+        redirect_appened = 1;
+        redirect_err = 0;
+        argv[i - 2] = NULL;
+        outfile = argv[i - 1];
+        cout << "outfile: " << outfile << endl;
+        }    
+    
     else 
-        redirect = 0; 
+        redirect_out = 0;
+        redirect_appened = 0;
+        redirect_err = 0;
+
     
 
     /* for commands not part of the shell command language */ 
 
     if (fork() == 0) { 
-        /* redirection of IO ? */
-        if (redirect) {
+        /* redirect_oution of IO ? */
+        if (redirect_out) {
             fd = creat(outfile, 0660); 
             close (STDOUT_FILENO) ; 
             dup(fd); 
             close(fd); 
-            /* stdout is now redirected */
+            /* stdout is now redirect_outed */
+        }
+        else if(redirect_appened){
+            fd = open(outfile, O_WRONLY | O_APPEND | O_CREAT);
+            close (STDOUT_FILENO) ;
+            dup(fd);
+            close(fd);
+        }
+        else if(redirect_err){
+            fd = creat(error_file, 0660);
+            close(STDERR_FILENO) ;
+            dup(fd);
+            close(fd);
         } 
+        /* execute command */
+        cout << "execvp: " << argv[0] << endl;
+
         execvp(argv[0], argv);
     }
     /* parent continues here */
     if (amper == 0)
         retid = wait(&status);
-
-    //need to write to stderr : hello: ls –l nofile 2> mylog
-    // need to impliment >> for appending to a file
+    
+    last_cmd_status= WEXITSTATUS(status);
     
 }
 }
