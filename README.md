6f31154bd62405ffe4ccdf0220eaffb2df40af30
TalMalchi <malchital@gmail.com>
2023-03-31 13:54:35 +0300

    comment in read me

--- README.md
+++ README.md
@@ -4,3 +4,24 @@
 ### How to Run? 
 
 In the terminal type `make clean`, and then `make` , to run type `./myshell` 
+
+Working : 
+1 : > , >> , 2>
+2 : prompt
+3 : echo 
+4 : echo $?
+5 : cd 
+7 : quit
+10 : Adding virables
+11 : read
+
+Need to do : 
+6 : !! command
+8 :  Control c -> print the message (You type control C), but doesnt print the prompt
+9 : pipe
+12 : arrow up & down ->  i start but it didnt work
+13 : IF/ELSE
+
+
+
+
--- shell2.cpp
+++ shell2.cpp
@@ -7,155 +7,318 @@
 #include "unistd.h"
 #include <string.h>
 #include <iostream>
+#include <map>
+#include <string>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+#define MAXHISTORY 20
 
 using namespace std;
 
-int main() {
-char command[1024];
-char *token;
-char *outfile, *error_file ;
-int i, fd, amper, redirect_out, retid, status, redirect_appened, redirect_err, last_cmd_status;
-char *argv[10];
-
-while (1)
+void handler(int sig)
 {
-    printf("hello: ");
-    fgets(command, 1024, stdin);
-    command[strlen(command) - 1] = '\0';
-
-    /* parse command line */
-    i = 0;
-    token = strtok (command," ");
-    while (token != NULL)
+    if (sig == SIGINT)
     {
-        argv[i] = token;
-        token = strtok (NULL, " ");
-        i++;
-    }
-    argv[i] = NULL;
-
-    /* Is command empty */
-    if (argv[0] == NULL)
-        continue;
-
-    /* Does command line end with & */ 
-    if (! strcmp(argv[i - 1], "&")) {
-        amper = 1;
-        argv[i - 1] = NULL;
-    }
-    else 
-        amper = 0; 
-
-    if (!strcmp(argv[0], "quit"))
-        exit(0);
-    
-    if (!strcmp(argv[0], "cd")) {
-        chdir(argv[1]);
-        continue;
-    }
-
-    if(!strcmp(argv[0], "echo") && !strcmp(argv[1], "$?")){
-        cout << last_cmd_status << endl;
-        continue;
-    }
-        
-
-    
-    if(!strcmp(argv[0], "echo")){
-        //print out the rest of the command line except for the echo command
-        for(int j = 1; j < i; j++){
-            cout << argv[j] << " ";
-        }
-        cout << endl; 
-        continue;
-    }
-
-    if (! strcmp(argv[i - 2], ">")) {
-        redirect_out = 1;
-        redirect_appened = 0;
-        redirect_err = 0;
-        argv[i - 2] = NULL;
-        outfile = argv[i - 1];
-        cout << "outfile: " << outfile << endl;
-        
-        
-        }
-    else if (! strcmp(argv[i - 2], "2>")) {
-        redirect_out = 0;
-        redirect_appened = 0;
-        redirect_err = 1;
-        argv[i - 2] = NULL;
-        error_file = argv[i - 1];
-        cout << "error_file: " << error_file << endl;
-        cout << "redirect_err: " << redirect_err << endl;
-        
-        
-        
-        }
-    else if (! strcmp(argv[i - 2], ">>")) {
-        redirect_out = 0;
-        redirect_appened = 1;
-        redirect_err = 0;
-        argv[i - 2] = NULL;
-        outfile = argv[i - 1];
-        cout << "outfile append: " << outfile << endl;
-        
-        }    
-    
-    else{
-        redirect_out = 0;
-        redirect_appened = 0;
-        redirect_err = 0;
-
-    }
-
-    /* for commands not part of the shell command language */ 
-
-    if (fork() == 0) { 
-        cout << "redirect inside fork: " << redirect_err << endl;
-        /* redirect_oution of IO ? */
-        if (redirect_out) {
-            fd = creat(outfile, 0660); 
-            close (STDOUT_FILENO) ; 
-            dup(fd); 
-            close(fd); 
-            /* stdout is now redirect_outed */
-        }
-         if(redirect_appened){
-            fd = open(outfile, O_WRONLY | O_APPEND | O_CREAT, 0660);
-            close (STDOUT_FILENO) ;
-            dup(fd);
-            close(fd);
-        }
-         if(redirect_err){
-            cout << "im here erorrr" << endl;
-            fd = creat(error_file, 0660);
-            close(STDERR_FILENO) ;
-            dup(fd);
-            close(fd);
-        } 
-        /* execute command */       
-            cout << "im here" << endl;
-            cout << "execvp: " << argv[0] << endl;
-            cout << "argv: " << argv << endl;
-            execvp(argv[0], argv);
-        
-    }
-    /* parent continues here */
-    else{
-        if (amper == 0){
-            retid = wait(&status);
-            if (retid < 0) {
-                    perror("waitpid() failed");
-                }
-                else {
-                    last_cmd_status = WEXITSTATUS(status);
-                }
-            
-        }
-        
-    
-
+        cout << "You typed Control-C!" << endl;
     }
 }
+int main()
+{
+    signal(SIGINT, handler);
+    char command[1024];
+    string prompt = "hello";
+    char *token;
+    char *outfile, *error_file;
+    int i, fd, amper, redirect_out, retid, status, redirect_appened, redirect_err, last_cmd_status;
+    char *argv[10];
+    char temp[1024];
+    int c = 0;
+    static map<string, string> variables;
+
+    try
+    {
+        while (1)
+        {
+            // string line;
+            cout << prompt << ": ";
+            fgets(command, 1024, stdin);
+            command[strlen(command) - 1] = '\0';
+            redirect_appened = 0;
+            redirect_out = 0;
+            redirect_err = 0;
+            cout << "command: " << command << endl;
+            c = command[0];
+
+            while (c == '\033')
+            {
+                cout << ("\033[1A"); // line up
+                cout << ("\x1b[2K"); // delete line
+                printf("got an arrow ");
+                switch (command[2])
+                { // the real value
+                case 'A':
+                    // code for arrow up
+                    cout << ("up");
+                    break;
+
+                case 'B':
+                    // code for arrow down
+                    cout << ("down");
+                    break;
+                }
+                char tempCommand[1024];
+                fgets(tempCommand, 1024, stdin);
+                tempCommand[strlen(tempCommand) - 1] = '\0';
+                c = tempCommand[0];
+                strcat(command, tempCommand);
+                cout << "command: " << command << endl;
+                if (c != '\033')
+                {
+                    break;
+                }
+                else
+                {
+                    cout << "command: inside else " << command << endl;
+                    strcpy(command, tempCommand);
+                }
+            }
+
+            /* parse command line */
+            i = 0;
+            token = strtok(command, " ");
+            while (token != NULL)
+            {
+                argv[i] = token;
+                token = strtok(NULL, " ");
+                i++;
+            }
+            argv[i] = NULL;
+
+            /* Is command empty */
+            if (argv[0] == NULL)
+            {
+                continue;
+            }
+
+            /* Does command line end with & */
+            if (!strcmp(argv[i - 1], "&"))
+            {
+                amper = 1;
+                argv[i - 1] = NULL;
+            }
+            else
+            {
+                amper = 0;
+            }
+
+            /* quit command */
+            if (!strcmp(argv[0], "quit"))
+            {
+                exit(0);
+            }
+
+            /* cd command */
+            if (!strcmp(argv[0], "cd"))
+            {
+                if (argv[1] == NULL)
+                {
+                    cout << "cd: expected argument to \"cd\"\n";
+                }
+                else if (chdir(argv[1]) != 0)
+                {
+                    perror("cd");
+                }
+
+                continue;
+            }
+            /* !! command */
+            if (!strcmp(argv[0], "!!"))
+            {
+                //TODO
+
+                continue;
+            }
+
+            /* prompt command */
+            if (!strcmp(argv[0], "prompt"))
+            {
+                cout << "prompt: " << argv[1] << endl;
+                if (i > 1 && !strcmp(argv[1], "="))
+                {
+                    if (argv[2] == NULL)
+                    {
+                        cout << "prompt: expected argument to \"prompt\"\n";
+                    }
+                    else
+                    {
+                        prompt = argv[2];
+                    }
+                }
+                continue;
+            }
+            /* set variable command */
+            if (argv[0][0] == '$' && !strcmp(argv[1], "="))
+            {
+                
+                string s = argv[0] + 1;
+                variables[s] = argv[2];
+                continue;
+            }
+
+            /* echo command */
+            if (!strcmp(argv[0], "echo"))
+            {
+                // check if the argument is empty
+                if (argv[1] == NULL)
+                {
+                    cout << "echo: expected argument to \"echo\"\n";
+                }
+
+                // check if the command is echo $?
+                if (!strcmp(argv[1], "$?")){
+                    cout << last_cmd_status << endl;
+                    continue;
+                }
+
+                for (int j = 1; j < i; j++)
+                {
+                    // check if the argument is a variable
+                    if (argv[j][0] == '$')
+                    {
+                        char *var = argv[j] + 1;
+                        if (variables.find(var) != variables.end())
+                        {
+                            cout << variables[var] << " ";
+                        }
+                    }
+                    // print out the rest of the command line except for the echo command
+                    else
+                    {
+                        cout << argv[j] << " ";
+                    }
+                }
+                cout << endl;
+                continue;
+            }
+
+            /* read command */
+            if (!strcmp(argv[0], "read"))
+            {
+                // check if the argument is empty
+                if (argv[1] == NULL)
+                {
+                    cout << "read: expected argument to \"read\"\n";
+                }
+                else
+                {
+                    string s;
+                    cin >> s;
+                    variables[argv[1]] = s;
+                    cin.ignore();
+                }
+                continue;
+            }
+
+            
+
+            if (!strcmp(argv[i - 2], ">"))
+            {
+                redirect_out = 1;
+                redirect_appened = 0;
+                redirect_err = 0;
+                argv[i - 2] = NULL;
+                outfile = argv[i - 1];
+            }
+            else if (!strcmp(argv[i - 2], "2>"))
+            {
+                redirect_out = 0;
+                redirect_appened = 0;
+                redirect_err = 1;
+                argv[i - 2] = NULL;
+                error_file = argv[i - 1];
+            }
+            else if (!strcmp(argv[i - 2], ">>"))
+            {
+                redirect_out = 0;
+                redirect_appened = 1;
+                redirect_err = 0;
+                argv[i - 2] = NULL;
+                outfile = argv[i - 1];
+            }
+
+            else
+            {
+                redirect_out = 0;
+                redirect_appened = 0;
+                redirect_err = 0;
+            }
+
+            /* for commands not part of the shell command language */
+            if (strcmp(argv[0], "cd") != 0 && strcmp(argv[0], "echo") != 0 && strcmp(argv[0], "prompt") != 0)
+            {
+                if (fork() == 0)
+                {
+                    /* redirect_oution of IO ? */
+                    if (redirect_out)
+                    {
+                        fd = creat(outfile, 0660);
+                        close(STDOUT_FILENO);
+                        dup(fd);
+                        close(fd);
+                        /* stdout is now redirect_outed */
+                    }
+                    if (redirect_appened)
+                    {
+                        fd = open(outfile, O_WRONLY | O_APPEND | O_CREAT, 0660);
+                        close(STDOUT_FILENO);
+                        dup(fd);
+                        close(fd);
+                    }
+                    if (redirect_err)
+                    {
+                        cout << "im here erorrr" << endl;
+                        fd = creat(error_file, 0660);
+                        close(STDERR_FILENO);
+                        dup(fd);
+                        close(fd);
+                    }
+
+                    /* execute command */
+                    else
+                    {
+                        execvp(argv[0], argv);
+
+                        /* if execvp returns, it failed */
+                        printf("Error executing command\n");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+            }
+            /* parent continues here */
+
+            if (amper == 0)
+            {
+                retid = wait(&status);
+                if (retid < 0)
+                {
+                    perror("waitpid() failed");
+                }
+                else
+                {
+                    last_cmd_status = WEXITSTATUS(status);
+                }
+            }
+        }
+    }
+    catch (exception &e)
+    {
+        cout << "An exception occurred. Exception Nr. " << e.what() << '\n';
+    }
 }
